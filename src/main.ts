ec3fc434b382b607651175b90d4cb785a2c3b255
R-unic <roleyspersonal@gmail.com>
2023-09-30 21:55:19 -0400

    fix: root directory not being optional in CLI

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { mkdirSync, readFileSync, writeFileSync } from "fs";
 import { exec } from "child_process";
 import { argv } from "process";
-import { boolean, command, flag, positional, run, string } from "cmd-ts";
+import { boolean, command, flag, optional, positional, run, string } from "cmd-ts";
 import path from "path";
 
 import Util from "./utility"
@@ -13,23 +13,21 @@ if (rootDirectory.endsWith(".js"))
   sliceIndex = 2
 
 const app = command({
-  name: "Crystallizer",
+  name: "crystallizer",
   args: {
-    rootDirectory: positional({ type: string, displayName: "Root directory", description: "Path to the main directory of your project" }),
+    rootDirectory: positional({ type: optional(string), displayName: "root directory", description: "Path to the main directory of your project" }),
     init: flag({ type: boolean, long: "init", short: "i", description: "Intialize a new Crystallizer project" })
   },
   handler: ({ rootDirectory, init }) => {
+    rootDirectory ??= path.relative(path.dirname(__dirname), process.cwd());
     if (init)
       initializeProject(rootDirectory);
-    else
-      compile(rootDirectory);
+    else {
+      const cli = new CLI(rootDirectory);
+      cli.runAll();
+    }
   }
 });
-
-function compile(rootDirectory: string) {
-  const cli = new CLI(rootDirectory ?? path.relative(path.dirname(__dirname), process.cwd()));
-  cli.runAll();
-}
 
 function initializeProject(rootDirectory: string) {
   exec(`git init`, { cwd: rootDirectory }, (error, stdout, stderr) => {
